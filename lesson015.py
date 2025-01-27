e602763df63426a88966fd3e6d55d14ebf409edf
Mark Peters <mapeters101@earthlink.net>
2025-01-26 22:48:59 -0500

    Work on lesson015 to

---
+++ lesson015.py
@@ -0,0 +1,65 @@
+from urllib.parse import parse_qsl
+
+
+class Node:
+    def __init__(self,value):
+        self.info = value
+        self.link = not None
+        
+class SingleLinkedList:
+    def __init__(self):
+        self.start = None
+        
+    def display_list(self):
+        pass
+    
+    def count_nodes(self):
+        pass
+    
+    def search(self,x):
+        pass
+    
+    def insert_in_beginning(self,data):
+        pass
+    
+    def insert_at_end(self,data):
+        pass
+    
+    def create_list(self):
+        pass
+    
+    def insert_after(self,data,x):
+        pass
+    
+    def insert_before(self,data,x):
+        pass
+    
+    def insert_at_position(self,data,k):
+        pass
+
+    def delete_node(self,x):
+        pass
+
+    def delete_first_node(self):
+        pass
+
+    def delete_last_node(self):
+        pass
+
+    def reverse_list(self):
+        pass
+
+    def bubble_sort_exdata(self):
+        pass
+
+    def bubble_sort_exlinks(self):
+        pass
+
+    def has_cycle(self):
+        pass
+
+    def find_cycle(self):
+        pass
+
+    def remove_cycle(self):
+        pass
