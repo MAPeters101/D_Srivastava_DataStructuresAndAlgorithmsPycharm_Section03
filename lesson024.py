a49cdc2322b3884686646347760c65d3bfb4a7a5
Mark Peters <mapeters101@earthlink.net>
2025-01-27 16:50:42 -0500

    Work on lesson024 to 8:40.  bubble_sort_exlinks()

--- lesson024.py
+++ lesson024.py
@@ -188,7 +188,22 @@ class SingleLinkedList:
             end = p
 
     def bubble_sort_exlinks(self):
-        pass
+        end = None
+        while end != self.start.link:
+            r = p = self.start
+            while p.link != end:
+                q = p.link
+                if p.info > q.info:
+                    p.link = q.link
+                    q.link = p
+                    if p != self.start:
+                        r.link = q
+                    else:
+                        self.start = q
+                    p,q = q,p
+                r = p
+                p = p.link
+            end = p
 
     def has_cycle(self):
         pass
