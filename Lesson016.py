198ac35e15b6577c8b9395414cc49b4cae833a5f
Mark Peters <mapeters101@earthlink.net>
2025-01-27 13:19:40 -0500

    Work on lesson016 to 4:25: count_nodes() function.

--- Lesson016.py
+++ Lesson016.py
@@ -21,7 +21,12 @@ class SingleLinkedList:
             print()
 
     def count_nodes(self):
-        pass
+        p = self.start
+        n = 0
+        while p is not None:
+            n+=1
+            p=p.link
+        print("Number of nodes in the list = ", n)
 
     def search(self, x):
         pass
