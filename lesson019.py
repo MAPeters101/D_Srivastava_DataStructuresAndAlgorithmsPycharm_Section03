4f6d7d812840ecde545689c81f4d399dd99419ab
Mark Peters <mapeters101@earthlink.net>
2025-01-27 14:09:58 -0500

    Work on code for lesson019 to 4:00: create_list() function.

--- lesson019.py
+++ lesson019.py
@@ -58,7 +58,12 @@ class SingleLinkedList:
         p.link = temp
 
     def create_list(self):
-        pass
+        n = int(input("Enter the number of nodes: "))
+        if n == 0:
+            return
+        for i in range(n):
+            data = int(input("Enter the element to be inserted: "))
+            self.insert_at_end(data)
 
     def insert_after(self, data, x):
         pass
